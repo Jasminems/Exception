9adcc2d0da7eeefc01e46bd82866e632362cc13d
Jasminems <jasmine.srakj@gmail.com>
2021-07-07 22:42:30 +0530

    Null pointer

---
+++ Nullexcept/src/Ex8.java
@@ -0,0 +1,16 @@
+/*A Java Program to generate NullPointer Exception*/
+public class Ex8 
+{
+	public static void main(String[] args) 
+	{
+        try 
+        {
+        	String s = null;
+            System.out.println(s.charAt(0));
+        } 
+        catch(NullPointerException e) 
+        {
+            System.out.println("NullPointerException");
+        }
+     }
+}
